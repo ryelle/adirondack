39c7e5e5cc3044e252a2e5d54378f28b4e779bc9
ryelle <kelly.dwan@gmail.com>
2014-08-28 19:00:05 -0400

    remove footer text (for now)

--- src/footer.php
+++ src/footer.php
@@ -10,13 +10,6 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'adirondack' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'adirondack' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'adirondack' ), 'Adirondack', '<a href="http://themes.redradar.net" rel="designer">Kelly Dwan & Mel Choyce</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>

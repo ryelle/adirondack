3db1249d9fd6614f312b22d0cbd7fc5bbbbaf87c
ryelle <kelly.dwan@gmail.com>
2015-04-08 10:48:35 -0400

    Revert "Use photon to CDN-ify the background images."
    This reverts commit 1c764c1e18aef0dcd16f453ab7146065fb99bd73.

--- src/content-single.php
+++ src/content-single.php
@@ -13,8 +13,7 @@
 	} elseif ( $url[2] < 800 ) { // Height
 		printf( '<div class="entry-image panorama">%s</div>', get_the_post_thumbnail( get_the_ID(), 'header-image' ) );
 	} else {
-		$url = apply_filters( 'jetpack_photon_url', $url[0], array( 'width' => 1400, 'zoom' => 2 ), null );
-		printf( '<div class="entry-image full-width" style="background-image:url(\'%s\');"></div>', esc_attr( $url ) );
+		printf( '<div class="entry-image full-width" style="background-image:url(\'%s\');"></div>', esc_attr( $url[0] ) );
 	}
 
 else: ?>
--- src/content.php
+++ src/content.php
@@ -7,10 +7,9 @@
 <?php if ( has_post_thumbnail() ) :
 	$image_id = get_post_thumbnail_id();
 	$url = wp_get_attachment_image_src( $image_id, 'post-thumbnail' );
-	$url = apply_filters( 'jetpack_photon_url', $url[0], array( 'resize' => array( 335, 250 ), 'zoom' => 2 ), null );
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background: url(<?php echo esc_attr( $url ); ?>) no-repeat center; background-size: cover;">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background: url(<?php echo esc_attr( $url[0] ); ?>) no-repeat center; background-size: cover;">
 	<a href="<?php the_permalink(); ?>" rel="bookmark" class="entry-link">
 		<header class="entry-header">
 			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
